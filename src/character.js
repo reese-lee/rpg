d6f86c2e240fdebedb3a4248cc1a2fef5bcb978e
Reese Lee and kaya jepson <reese+kaya@>
2019-05-30 08:54:59 -0700

    add Sheep character class and constructor

---
+++ src/character.js
@@ -0,0 +1,11 @@
+export class Sheep {
+  constructor(name, level, intelligence) {
+    this.name = name;
+    this.level = level;
+    this.intelligence = intelligence;
+  }
+
+  checkLevel() {
+    let level = 0;
+  }
+}
